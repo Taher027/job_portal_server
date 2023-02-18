ade730e763153e32bcb6c720dd67c768a37fb58f
Taher027 <abutaherh27@gmail.com>
2023-02-18 15:43:40 +0600

    create server connect to mongodb get data from mongodb

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -9,6 +10,27 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iitoxlh.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const jobCollection = client.db("job_portal").collection("jobCollections");
+    app.get("/jobcollections", async (req, res) => {
+      const query = {};
+      const jobs = await jobCollection.find(query).toArray();
+      res.send(jobs);
+    });
+  } finally {
+  }
+}
+run().catch(console.log);
 app.get("/", async (req, res) => {
   res.send("job portal is running on port 5000");
 });
